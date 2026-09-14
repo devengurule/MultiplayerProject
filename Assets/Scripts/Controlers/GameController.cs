7ae7a6235503ad9e63f63184d80188c6a252d74e
devengurule <dgurule3@uccs.edu>
2026-09-13 18:42:24 -0600

    Deleted Old Lines of Code

--- Assets/Scripts/Controlers/GameController.cs
+++ Assets/Scripts/Controlers/GameController.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    //[field: SerializeField, Range(0f, 1f)] public float lightLossPercent { get; private set; }
-    //[field: SerializeField, Range(0f, 1f)] public float heavyLossPercent { get; private set; }
     [field: SerializeField] public int maxPlayerHealth { get; private set; }
     [field: SerializeField] public int bulletDamage { get; private set; }
     [field: SerializeField] public float knockbackForce { get; private set; }
