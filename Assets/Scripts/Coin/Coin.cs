d983143be2cf7f6e7bc6954dc2282a470c745092
devengurule <dgurule3@uccs.edu>
2026-09-18 11:19:41 -0600

    Added Controls Board and Death Particles to Coin

--- Assets/Scripts/Coin/Coin.cs
+++ Assets/Scripts/Coin/Coin.cs
@@ -1,9 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem deathParticles;
+
     public void DestroySequence()
     {
-        Destroy(gameObject);
+        foreach (SphereCollider collider in gameObject.GetComponents<SphereCollider>())
+        {
+            collider.enabled = false;
+        }
+
+        foreach (MeshRenderer meshRenderer in gameObject.GetComponentsInChildren<MeshRenderer>())
+        {
+            if (meshRenderer != null) meshRenderer.enabled = false;
+        }
+
+        gameObject.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+
+        StartCoroutine(PlayDeathParticles());
+    }
+
+    private IEnumerator PlayDeathParticles()
+    {
+        deathParticles.Play();
+
+        while (deathParticles.isEmitting)
+        {
+            yield return null;
+        }
+
+        Destroy(this.gameObject);
     }
 }
